0d975e01aea99019110096df6ee09df6f68915d2
Jose Torres <joseangel.tosa@outlook.com>
2024-11-07 22:30:46 -0600

    Detallando

--- index.php
+++ index.php
@@ -29,7 +29,8 @@
             <a href="#tiempo" class="nav-link" data-bs-toggle="tab">Tiempo</a>
         </li>
     </ul>
-
+    <p class="text-start">Ingresa el dato en la unidad de medida que necesites,
+        las otras unidades se convertiran automaticamente.</p>
     <button class="btn btn-danger mt-3" onclick="clearInputs()">Limpiar Inputs</button>
 
 
@@ -39,104 +40,104 @@
                 <div class="col-6">Kilómetro (km) <input type="number" class="form-control" id="input-km"
                         placeholder="Ingresa tu dato" oninput="convertLength()"></div>
                 <div class="col-6">Metro (m) <input type="number" class="form-control" id="input-m" value="0"
-                        oninput="convertLength()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertLength()"></div>
                 <div class="col-6">Milla (mi) <input type="number" class="form-control" id="input-mi" value="0"
-                        oninput="convertLength()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertLength()"></div>
                 <div class="col-6">Centímetro (cm) <input type="number" class="form-control" id="input-cm" value="0"
-                        oninput="convertLength()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertLength()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="masa">
             <div class="row">
                 <div class="col-6">Tonelada (t) <input type="number" class="form-control" id="input-t" value="0"
-                        oninput="convertMass()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertMass()"></div>
                 <div class="col-6">Kilogramo (kg) <input type="number" class="form-control" id="input-kg" value="0"
-                        oninput="convertMass()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertMass()"></div>
                 <div class="col-6">Libra (lb) <input type="number" class="form-control" id="input-lb" value="0"
-                        oninput="convertMass()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertMass()"></div>
                 <div class="col-6">Onza (oz) <input type="number" class="form-control" id="input-oz" value="0"
-                        oninput="convertMass()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertMass()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="volumen">
             <div class="row">
                 <div class="col-6">Metro cúbico (m³) <input type="number" class="form-control" id="input-m3" value="0"
-                        oninput="convertVolume()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertVolume()"></div>
                 <div class="col-6">Litro (L) <input type="number" class="form-control" id="input-L" value="0"
-                        oninput="convertVolume()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertVolume()"></div>
                 <div class="col-6">Galón (gal) <input type="number" class="form-control" id="input-gal" value="0"
-                        oninput="convertVolume()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertVolume()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="temperatura">
             <div class="row">
                 <div class="col-6">Celsius (°C) <input type="number" class="form-control" id="input-celsius" value="0"
-                        oninput="convertTemperature()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertTemperature()"></div>
                 <div class="col-6">Fahrenheit (°F) <input type="number" class="form-control" id="input-fahrenheit"
-                        value="0" oninput="convertTemperature()"></div>
+                        placeholder="Ingresa tu dato" value="0" oninput="convertTemperature()"></div>
                 <div class="col-6">Kelvin (K) <input type="number" class="form-control" id="input-kelvin" value="0"
-                        oninput="convertTemperature()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertTemperature()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="energia">
             <div class="row">
                 <div class="col-6">Kilovatio-hora (kWh) <input type="number" class="form-control" id="input-kWh"
-                        value="0" oninput="convertEnergy()"></div>
+                        placeholder="Ingresa tu dato" value="0" oninput="convertEnergy()"></div>
                 <div class="col-6">Joule (J) <input type="number" class="form-control" id="input-J" value="0"
-                        oninput="convertEnergy()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertEnergy()"></div>
                 <div class="col-6">BTU <input type="number" class="form-control" id="input-BTU" value="0"
-                        oninput="convertEnergy()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertEnergy()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="area">
             <div class="row">
                 <div class="col-6">Metro cuadrado (m²) <input type="number" class="form-control" id="input-m2" value="0"
-                        oninput="convertArea()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertArea()"></div>
                 <div class="col-6">Kilómetro cuadrado (km²) <input type="number" class="form-control" id="input-km2"
-                        value="0" oninput="convertArea()"></div>
+                        placeholder="Ingresa tu dato" value="0" oninput="convertArea()"></div>
                 <div class="col-6">Hectárea (ha) <input type="number" class="form-control" id="input-ha" value="0"
-                        oninput="convertArea()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertArea()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="velocidad">
             <div class="row">
                 <div class="col-6">Kilómetro por hora (km/h) <input type="number" class="form-control" id="input-kmph"
-                        value="0" oninput="convertSpeed()"></div>
+                        placeholder="Ingresa tu dato" value="0" oninput="convertSpeed()"></div>
                 <div class="col-6">Metro por segundo (m/s) <input type="number" class="form-control" id="input-mps"
-                        value="0" oninput="convertSpeed()"></div>
+                        placeholder="Ingresa tu dato" value="0" oninput="convertSpeed()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="datos">
             <div class="row">
                 <div class="col-6">Kilobyte (KB) <input type="number" class="form-control" id="input-KB" value="0"
-                        oninput="convertData()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertData()"></div>
                 <div class="col-6">Megabyte (MB) <input type="number" class="form-control" id="input-MB" value="0"
-                        oninput="convertData()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertData()"></div>
                 <div class="col-6">Gigabyte (GB) <input type="number" class="form-control" id="input-GB" value="0"
-                        oninput="convertData()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertData()"></div>
                 <div class="col-6">Terabyte (TB) <input type="number" class="form-control" id="input-TB" value="0"
-                        oninput="convertData()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertData()"></div>
             </div>
         </div>
 
         <div class="tab-pane fade" id="tiempo">
             <div class="row">
                 <div class="col-6">Segundo (s) <input type="number" class="form-control" id="input-s" value="0"
-                        oninput="convertTime()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertTime()"></div>
                 <div class="col-6">Minuto (min) <input type="number" class="form-control" id="input-min" value="0"
-                        oninput="convertData()">
+                        placeholder="Ingresa tu dato" oninput="convertData()">
                 </div>
                 <div class="col-6">Hora (h) <input type="number" class="form-control" id="input-h"
-                        value="0">oninput="convertData()"</div>
+                        placeholder="Ingresa tu dato" value="0">oninput="convertData()"</div>
                 <div class="col-6">Día <input type="number" class="form-control" id="input-dia" value="0"
-                        oninput="convertData()"></div>
+                        placeholder="Ingresa tu dato" oninput="convertData()"></div>
             </div>
         </div>
     </div>
